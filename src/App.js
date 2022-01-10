4932fbd8ef9fada9255ec89ba8a4f31fa5059271
Josiah Etuk <etukjosiahbenjamin@gmail.com>
2022-01-10 18:51:03 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Helmet } from "react-helmet";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Yoba</title>
+        <meta name="description" content="React application" />
+      </Helmet>
+      <section className="container">
+        Yoba
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
