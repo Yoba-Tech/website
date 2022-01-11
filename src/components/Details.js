d93d365fa66749f926a8f9b96af186baeb2ff49e
Josiah Etuk <etukjosiahbenjamin@gmail.com>
2022-01-11 10:03:38 +0100

    after image

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,9 +2,25 @@
 import banks from "../images/banks.png"
 import banks2 from "../images/banks2.png"
 import banks3 from "../images/banks3.png"
+import svg from "../images/svg.png"
+import svgSm from "../images/svgSm.png"
 import { motion } from "framer-motion"
+import { useWindowSize, useMediaQuery, useOnScreen } from "../hooks"
+import { useEffect, useState, useRef } from "react"
 
 const Details = () => {
+    const windowSize = useWindowSize()
+    const isPhone = useMediaQuery("(max-width: 768px)")
+    const [right, setRight] = useState(0)
+
+    const ref = useRef()
+    const inView = useOnScreen(ref);
+
+    useEffect(() => {
+        const details = document.querySelector('.details')
+        setRight(details.getBoundingClientRect().left)
+    }, [windowSize, inView, right])
+
     return (
         <div className="container">
             <section className="details">
@@ -36,7 +52,7 @@ const Details = () => {
                     </motion.div>
                     <div className="col-text">
                         <h2>
-                            One physical card tied to all bank accounts
+                            Add your bank account and spend straight from Yoba
                         </h2>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis vel in mollis quis euismod in aliquam facilisi purus.
@@ -49,7 +65,7 @@ const Details = () => {
                 <div className="row">
                     <div className="col-text">
                         <h2>
-                            One physical card tied to all bank accounts
+                            Have access to a dollar with a large limit.
                         </h2>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis vel in mollis quis euismod in aliquam facilisi purus.
@@ -65,6 +81,9 @@ const Details = () => {
                         <img src={banks3} width={499} height={420} alt="bank card" />
                     </motion.div>
                 </div>
+
+                {!isPhone ? <img style={{ right: `-${right}px` }} className="img" src={svg} alt="svg" /> :
+                    <img style={{ right: `-${right}px` }} className="img" src={svgSm} alt="svg" />}
             </section>
         </div>
     )
