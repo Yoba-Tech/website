739de132d7b0d9930ddc306e08985e0a78b887b2
Josiah Etuk <etukjosiahbenjamin@gmail.com>
2022-01-11 08:38:57 +0100

    font addition

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import useMediaQuery from "./useMediaQuery.js";
 import useWindowSize from "./useWindowSize.js";
 import useOnScreen from "./useOnScreen.js";
+import useWindowDimensions from "./useWindowDimensions.js";
 
-export { useMediaQuery, useWindowSize, useOnScreen };
+export { useMediaQuery, useWindowSize, useOnScreen, useWindowDimensions };
