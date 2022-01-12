52a7227f82ab63970e10d2dde185a526643c88a8
Josiah Etuk <etukjosiahbenjamin@gmail.com>
2022-01-12 12:27:39 +0100

    HERO GQL

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/export.scss';
 import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { ContentfulRestLink } from 'apollo-link-contentful'
+
+const space = process.env.REACT_APP_CONTENTFUL_SPACE
+const accessToken = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+
+const client = new ApolloClient({
+  link: new ContentfulRestLink({
+    space,
+    accessToken,
+  }, {
+    include: 10,
+  }),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
